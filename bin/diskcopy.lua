392619cd7702d69ef40a7b478172c2aea9e920a9
afonya2 <64413731+afonya2@users.noreply.github.com>
2021-10-10 20:21:04 +0200

    Update diskcopy.lua

--- bin/diskcopy.lua
+++ bin/diskcopy.lua
@@ -7,12 +7,12 @@ end
 io.write("enter target disk first 3 letter of id or none to cancel copy! ")
 local szar = io.read()
 if szar ~= "none" then
-    exec("cp /autorun.lua /mnt/"..szar.."/")
-    exec("cp /bin /mnt/"..szar.."/")
-    exec("cp /boot /mnt/"..szar.."/")
-    exec("cp /etc /mnt/"..szar.."/")
-    exec("cp /home /mnt/"..szar.."/")
-    exec("cp /lib /mnt/"..szar.."/")
+    exec("cp /autorun.lua /mnt/"..szar.."/ -r")
+    exec("cp /bin /mnt/"..szar.."/ -r")
+    exec("cp /boot /mnt/"..szar.."/ -r")
+    exec("cp /etc /mnt/"..szar.."/ -r")
+    exec("cp /home /mnt/"..szar.."/ -r")
+    exec("cp /lib /mnt/"..szar.."/ -r")
 else
     print("Back to shell")
 end
